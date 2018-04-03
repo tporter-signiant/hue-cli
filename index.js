eb493f5eb73100f8c4dda531c8b632204c83ab86
Trevor Porter <tporter@signiant.com>
2018-04-02 20:40:49 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+const program = require('commander');
+const {HueApi, lightState} = require('node-hue-api');
+const effects = require('./effects');
+
+program
+    .option('--hostname <hostname>', 'Bridge hostname')
+    .option('--username <username>', 'Bridge username')
+    .option('--group-id <group-id>', 'Light group to target')
+    .option('--effect <effect-name>', 'Light effect to apply to group')
+    .parse(process.argv);
+
+const hueApiClient = new HueApi(program.hostname, program.username);
+
+const toLightState = (stateObj) => {
+    let state = lightState.create();
+    state = stateObj.on ? state.on() : state.off();
+    return state.bri(stateObj.bri).hue(stateObj.hue).sat(stateObj.sat).transition(stateObj.transition || 1000);
+};
+
+const getGroupLightStates = async (groupId) => {
+    const {lights} = await hueApiClient.getGroup(groupId);
+    const lightStatuses = await Promise.all(lights.map(light => hueApiClient.lightStatus(light)));
+
+    return lightStatuses.map((lightStatus, index) => ({
+        id: lights[index],
+        state: toLightState(lightStatus.state)
+    }));
+};
+
+const setLightStates = async (lightStates) =>
+    Promise.all(lightStates.map(lightState => hueApiClient.setLightState(lightState.id, lightState.state)));
+
+const delay = (delayInMs) => () => new Promise(function(resolve) {
+    setTimeout(resolve, delayInMs);
+});
+
+(async function () {
+    const queue = [];
+    const lightStates = await getGroupLightStates(program.groupId);
+
+    effects[program.effect].forEach((effectState) => {
+        const state = toLightState(effectState);
+        queue.push(() => hueApiClient.setGroupLightState(program.groupId, state));
+        queue.push(delay(effectState.transition));
+    });
+
+    let next;
+
+    while (next = queue.shift()) {
+        await next();
+    }
+
+    await setLightStates(lightStates);
+})();
